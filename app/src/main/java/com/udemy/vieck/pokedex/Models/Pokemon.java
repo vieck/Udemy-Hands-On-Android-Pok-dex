94898b117bc08fd4d529bba70d47be077c959779
vieck <vieck@purdue.edu>
2017-12-28 17:08:08 -0500

    Added pokemon model.

---
+++ app/src/main/java/com/udemy/vieck/pokedex/Models/Pokemon.java
@@ -0,0 +1,11 @@
+package com.udemy.vieck.pokedex.Models;
+
+public class Pokemon {
+   public int id;
+   public String name;
+   public int base_experience;
+   public int height;
+   public boolean is_default;
+   public int order;
+   public int weight;
+}
