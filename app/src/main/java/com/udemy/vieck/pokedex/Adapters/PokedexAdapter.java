a0055b79ff760e09e7b89bd42776a6eacdf3eae9
vieck <vieck@purdue.edu>
2018-01-10 15:31:00 -0500

    Started adapter for the pokedex cards.

---
+++ app/src/main/java/com/udemy/vieck/pokedex/Adapters/PokedexAdapter.java
@@ -0,0 +1,22 @@
+package com.udemy.vieck.pokedex.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+public class PokedexAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
