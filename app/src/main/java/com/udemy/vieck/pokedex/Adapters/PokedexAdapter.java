e68204bb964c393f5a73b9ac75d9141e7973efe0
vieck <vieck@purdue.edu>
2018-01-12 11:49:24 -0500

    Created the viewholder and integrated it into the adapter.

--- app/src/main/java/com/udemy/vieck/pokedex/Adapters/PokedexAdapter.java
+++ app/src/main/java/com/udemy/vieck/pokedex/Adapters/PokedexAdapter.java
@@ -1,22 +1,53 @@
 package com.udemy.vieck.pokedex.Adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.udemy.vieck.pokedex.Models.PokemonResource;
+import com.udemy.vieck.pokedex.R;
+
+import java.util.List;
 
 
-public class PokedexAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class PokedexAdapter extends RecyclerView.Adapter<PokedexAdapter.PokedexViewHolder> {
+
+    private List<PokemonResource> pokemonResources;
+
+    public PokedexAdapter(List<PokemonResource> pokemonResources) {
+        this.pokemonResources = pokemonResources;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public PokedexViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_pokedex, parent, false);
+        return new PokedexViewHolder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(PokedexViewHolder holder, int position) {
+        PokemonResource pokemonResource = pokemonResources.get(position);
+        holder.pokemonNameText.setText(pokemonResource.name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return pokemonResources.size();
+    }
+
+    public class PokedexViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView pokemonImageView;
+        private TextView pokemonNameText;
+
+        public PokedexViewHolder(View itemView) {
+            super(itemView);
+            pokemonImageView = (ImageView) itemView.findViewById(R.id.pokemon_image);
+            pokemonNameText = (TextView) itemView.findViewById(R.id.pokemon_name);
+        }
     }
 }
