6e95b53475b2e49e5f390fec5923d826c9acca7b
vieck <vieck@purdue.edu>
2018-01-14 09:59:28 -0500

    Started the Pokemon Description Activity.

---
+++ app/src/main/java/com/udemy/vieck/pokedex/Activities/PokemonDescActivity.java
@@ -0,0 +1,15 @@
+package com.udemy.vieck.pokedex.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udemy.vieck.pokedex.R;
+
+public class PokemonDescActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_desc);
+    }
+}
