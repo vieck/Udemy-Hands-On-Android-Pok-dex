801081ebb98795f26ddcc8a69800e06d5454dd33
vieck <vieck@purdue.edu>
2018-01-18 12:37:51 -0500

    Built pokemon image views.

--- app/src/main/java/com/udemy/vieck/pokedex/Models/Pokemon.java
+++ app/src/main/java/com/udemy/vieck/pokedex/Models/Pokemon.java
@@ -11,13 +11,9 @@ public class Pokemon {
 
    public class PokemonSprite {
       public String front_default;
-      public String front_shiny;
       public String front_female;
-      public String front_shiny_female;
       public String back_default;
-      public String back_shiny;
       public String back_female;
-      public String back_shiny_female;
    }
 
 }
