871c2b738390f6e6fb51f515f6539a1b87321f57
vieck <vieck@purdue.edu>
2018-02-04 11:24:58 -0500

    Created PokemonMove model.

---
+++ app/src/main/java/com/udemy/vieck/pokedex/Models/PokemonMove.java
@@ -0,0 +1,6 @@
+package com.udemy.vieck.pokedex.Models;
+
+public class PokemonMove {
+    public NamedAPIResource move;
+    public PokemonMoveVersion version_group_details;
+}
---
+++ app/src/main/java/com/udemy/vieck/pokedex/Models/PokemonMoveVersion.java
@@ -0,0 +1,7 @@
+package com.udemy.vieck.pokedex.Models;
+
+public class PokemonMoveVersion {
+    public NamedAPIResource move_learn_method;
+    public NamedAPIResource version_group;
+    public int level_learned_at;
+}
